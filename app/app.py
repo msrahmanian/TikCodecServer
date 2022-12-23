fe540a1a3b2fc4369add388163d17dc36c8b0b45
rahmanian <rahmanian@gmail.com>
2022-12-23 16:22:54 +0330

    dev

--- app/app.py
+++ app/app.py
@@ -13,6 +13,9 @@ s3_resource = boto3.resource(
         aws_access_key_id='1cc43c48-00ce-4b80-a709-dd0034d8990f',
         aws_secret_access_key='2ccdafd36f0624a03bc05a63d26641f10cd6dd236f4d5cca6f516d3eb5a88b97'
     )
+
+
+    
 def upload_single_object(file:str,outfile:str,uid:str):
     try:
         bucket_name = "mymedia" #s3 bucket name
@@ -39,19 +42,20 @@ def upload_objects(input:str,uid:str):
         print(err)
 
 app = Flask(__name__)
-def ffmpegToHSV(input:str,uid:str,scale:int):
+def ffmpegToHSV(input:str,uid:str,width:int,height:int):
         path1 = os.path.join(uid)
         if not os.path.isdir(path1):
             os.mkdir(path1)
         
-        path2=os.path.join(path1,str(scale))
+        path2=os.path.join(path1,str(width))
         if not os.path.isdir(path2):
             os.mkdir(path2)
         input = ffmpeg.input(input, f='mp4')
         audio = input.audio
-        video = input.video.filter('scale', -1, scale)
-        output_stream = ffmpeg.output(video,audio, path2 + '/' + str(scale) + '.m3u8', format='hls', start_number=0, hls_time=2, hls_list_size=1000000)
+        video = input.video.filter('scale', height, width)
+        output_stream = ffmpeg.output(video,audio, path2 + '/' + str(width) + '.m3u8', acodec='aac',strict='experimental',aspect='16:9', vcodec='libx264', format='hls', start_number=0, hls_time=2, hls_list_size=1000000)
         ffmpeg.run(output_stream)
+        
         upload_objects(path2,uid)
         try:
             shutil.rmtree(path2)
@@ -63,14 +67,16 @@ def convert_hsv_by_url():
     try:        
        url= request.args.get("url")
        uuidFour = uuid.uuid4()     
-       p1 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),240))
-       p2 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),360))  
-       p3 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),480))
-       
+       p1 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),240,320))
+       p2 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),360,640))  
+       p3 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),480,800))
+       p4 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),540,960))
+       p5 = Process(target=ffmpegToHSV, args=(url,str(uuidFour),720,1280))
        p1.start()
        p2.start()
        p3.start()
-       
+       p4.start()
+       p5.start()
        upload_single_object('playlist.m3u8','playlist.m3u8',str(uuidFour))
 
        return str(uuidFour)
@@ -78,23 +84,6 @@ def convert_hsv_by_url():
         print(e.stderr.decode(), file=sys.stderr)
 
 
-
-
-@app.route('/get_thumb')
-def get_image():
-    try:
-        (
-            ffmpeg
-            .input('https://s3.tikschool.ir/sample.mp4', ss=0.5)
-            .filter('scale', 1200, -1)
-            .output('out.jpg', vframes=1)
-            .overwrite_output()
-            .run(capture_stdout=True, capture_stderr=True)
-            
-        )
-        return send_file('../out.jpg', mimetype='image/jpg')
-    except ffmpeg.Error as e:
-        print(e.stderr.decode(), file=sys.stderr)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
